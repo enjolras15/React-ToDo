52c0031b2f843e5f2fe743bab4bdd8084a5fab70
enjolras <durandal0425@gmail.com>
2024-03-05 16:10:29 +0900

    update

--- source/program.js
+++ source/program.js
@@ -8,6 +8,21 @@
                 this.addTodo = this.addTodo.bind(this);
                 this.toggleTodo = this.checkTodo.bind(this);
                 this.deleteTodo = this.deleteTodo.bind(this);
+                this.handleDragStart = this.handleDragStart.bind(this);
+                this.handleDragEnd = this.handleDragEnd.bind(this);
+                this.handleDrop = this.handleDrop.bind(this);
+            }
+
+            handleDragStart(event, id) {
+
+            }
+
+            handleDragEnd(event) {
+
+            }
+
+            handleDrop(event) {
+
             }
 
             componentDidMount() {
@@ -60,11 +75,16 @@
                         React.createElement('button', { onClick: this.addTodo }, 'Add Todo'),
                         React.createElement('div', { className:'box' },
                             todos.map(todo =>
-                                React.createElement('li', { key: todo.id },
+                                React.createElement('li', {
+                                    draggable: "true",
+                                    ondragstart: (event) => this.handleDragStart(event,todo.id),
+                                    ondragend: (event) => this.handleDragEnd(event),
+                                    ondrop: (event) => this.handleDrop(event)},
+     
                                     React.createElement('input', {
                                         type: 'checkbox',
                                         checked: todo.completed,
-                                        onChange: () => this.checkTodo(todo.id)
+                                        onChange: () => this.checkTodo(todo.id),
                                     }),
                                     React.createElement('span', 
                                         null
